df53e27210a00b6fb2688b0e7dc3ba85a27cff8c
hbasaran23 <59734345+hbasaran23@users.noreply.github.com>
2020-01-28 13:38:53 -0300

    Update Programming Assignment 2 Lexical Scoping.R

--- Programming Assignment 2 Lexical Scoping.R
+++ Programming Assignment 2 Lexical Scoping.R
@@ -44,11 +44,6 @@ my_matrix$get()
 my_matrix$getInverse()
 cacheSolve(my_matrix)
 cacheSolve(my_matrix)
-my_matrix$getInverse()
-my_matrix$set(matrix(c(2, 2, 1, 4), 2, 2))
-my_matrix$get()
-my_matrix$getInverse()
-cacheSolve(my_matrix)
-cacheSolve(my_matrix)
-my_matrix$getInverse()
+
+
                              
